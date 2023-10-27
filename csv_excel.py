f4862b0593df5099b5b462310ab7ed9dacc0895d
Lucia <lucia.esperon@udc.es>
2023-10-27 10:45:38 +0200

    mejorado

--- csv_excel.py
+++ csv_excel.py
@@ -2,14 +2,27 @@ import pandas as pd
 
 
 def mostrar_archivocsv():
-    ruta_csv = input('Añade la ruta de tu archivo CSV: ')
-    df = pd.read_csv(ruta_csv)
-    print(df.head(10))
+    try:
+        ruta_csv = input('Añade la ruta de tu archivo CSV: ')
+        df = pd.read_csv(ruta_csv)
+        print(df.head(10))
+    except FileNotFoundError:
+        print('El archivo CSV no se encuentra en la ruta elegida')
+    except Exception as j:
+        print(f"Se produjo un error al cargar el archivo CSV: {str(j)}")
 mostrar_archivocsv()
 
 def mostrar_archivoexcel():
-    ruta_xlsx = input('Añade la ruta de tu archivo XLSX: ')
-    df = pd.read_excel(ruta_xlsx)
-    print(df.head(10))
+    try:
+        ruta_xlsx = input('Añade la ruta de tu archivo XLSX: ')
+        df = pd.read_excel(ruta_xlsx)
+        print(df.head(10))
+    except FileNotFoundError:
+        print('El archivo XLSX no se encuentra en la ruta elegida')
+    except Exception as j:
+        print(f"Se produjo un error al cargar el archivo XLSX: {str(j)}")
+
 mostrar_archivoexcel() 
+
+
 
