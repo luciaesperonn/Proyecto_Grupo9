eefbf57be182e27fdc0871353c7a0cfe1c5b5d82
Lucia <lucia.esperon@udc.es>
2023-11-16 13:51:00 +0100

    hay que arreglar una cosa

--- interfaz+almac.py
+++ interfaz+almac.py
@@ -13,10 +13,13 @@ button_back = None
 button_examine = None
 
 def browse_files():
+    global archivo_seleccionado
+
     filename = filedialog.askopenfilename(initialdir="/", title="Examinar", filetypes=(("Text files", "*.txt*"), ("CSV files", "*.csv"), ("Excel files", "*.xlsx"), ("SQLite databases", "*.db"), ("all files", "*.*")))
 
     if filename:  # Verificar si se seleccionó un archivo
-        label_file_explorer.configure(text="Archivo abierto: ")
+        archivo_seleccionado=filename
+        label_file_explorer.configure(text="Archivo abierto:  ")
         
         # Habilitar el botón "Salir"
         button_exit.config(state=tk.NORMAL)
@@ -28,6 +31,11 @@ def browse_files():
         global button_examine
         button_examine = tk.Button(window, text="Examinar", command=lambda: toggle_examine(filename), height=1, width=16)
         button_examine.place(x=200, y=230)
+
+        #Llamar a la función para crear los Radiobuttons
+        radiobuttons_var1 = create_radiobuttons(window, var1, filename, 380, Seleccionar)
+        radiobuttons_var2 = create_radiobuttons(window, var2, filename, 440, Seleccionar)
+
 
 # Nueva función para alternar la visibilidad de la ruta del archivo
 def toggle_examine(filename):
@@ -71,23 +79,51 @@ def show_error(message):
     text.insert(tk.INSERT, message)
     text.pack()
 
-def realizar_regresion_lineal(archivo, columna_x, columna_y):
-    datos = mostrar_archivos(archivo)
-    modelo = crear_modelo_regresion_lineal(archivo, [columna_x], [columna_y])
-    datos = datos.dropna(subset=[columna_x, columna_y])
 
-    X = datos[[columna_x]]
-    y = datos[columna_y]
 
-    visualizar_modelo(modelo, X, y, [columna_x])
 
-def realizar_regresion_button():
-    variable_x = var1.get()
-    variable_y = var2.get()
+#Función para seleccionar las columnas de los datos que se usarán como entradas y salida del modelo
+def Seleccionar():
+   global archivo_seleccionado
+   variable_x = var1.get()
+   variable_y = var2.get()
 
-    # Verifica que se hayan seleccionado ambas variables
-    if variable_x != ' ' and variable_y != ' ':
-        realizar_regresion_lineal("housing.csv", variable_x, variable_y)
+   if variable_x != ' ' and variable_y != ' ' and archivo_seleccionado:
+       try:
+           modelo = crear_modelo_regresion_lineal(archivo_seleccionado, [variable_x], [variable_y])
+
+           # Obtener los datos correspondientes a las variables seleccionadas
+           datos_seleccionados = mostrar_archivos(archivo_seleccionado)
+           datos_seleccionados = datos_seleccionados.dropna(subset=[variable_x, variable_y])
+           X_seleccionado = datos_seleccionados[[variable_x]]
+           y_seleccionado = datos_seleccionados[variable_y]
+
+           visualizar_modelo(modelo, X_seleccionado, y_seleccionado, [variable_x])
+       except Exception as e:
+           show_error(f"Error al crear o visualizar el modelo: {str(e)}")
+
+def create_radiobuttons(window,variable, filename, y_position, command_callback):
+    radiobuttons = []
+    #Obtener la primera fila del DataFrame
+    primera_fila = get_first_row(filename)
+    if primera_fila is not None:
+        for i, (columna,value) in enumerate(primera_fila.items()):
+            rad = Radiobutton(window, variable=variable, value=columna, text=i, command=command_callback)
+            rad.pack(side=LEFT)
+            rad.place(x=80+120*i, y=y_position)
+            rad.config(bg="#d9ffdf")
+            radiobuttons.append(rad)
+    else:
+        print("Error al obtener la primera fila del archivo.")
+    return radiobuttons
+
+def get_first_row(filename):
+    df = mostrar_archivos(filename)
+    if df is not None:
+        primera_fila = df.iloc[0]
+        return primera_fila
+    else:
+        return None
 
 
 
@@ -97,8 +133,14 @@ window.title('EXPLORADOR DE ARCHIVOS')
 window.geometry("500x500")
 window.config(bg="#bcdbf3")
 
-# Crear elementos de la interfaz gráfica
+# Creación de los Radiobutton
+var1 = StringVar()
+var2 = StringVar()
+# Establecer valores predeterminados
+var1.set(' ')
+var2.set(' ')
 
+# Crear elementos de la interfaz gráfica
 label_file_explorer = tk.Label(window, text="Explorador de Archivos usando Tkinter", width=250, height=5, fg="black", bg="#d9ffdf")
 button_explore = tk.Button(window, text="Buscar Archivos", command=browse_files, height=1, width=16)
 button_exit = tk.Button(window, text="Salir", command=window.quit, height=1, width=6)
@@ -112,11 +154,8 @@ button_exit.place(x=230, y=260)
 listbox_resultado = tk.Listbox(window, selectmode=tk.SINGLE, height=1, width=250, bg="#dfe9f5")
 listbox_resultado.place(y=300)
 
-#Función para seleccionar las columnas de los datos que se usarán como entradas y salida del modelo
-def Seleccionar():
-   print(var1.get())
-   print(var2.get())
-   print()
+button_regression = tk.Button(window, text="Realizar Regresión Lineal", command=Seleccionar, height=1, width=20)
+button_regression.place(x=180, y=470)
 
 #Etiquetas
 etiqueta_seleccionar = tk.Label(window, text="Selecciona una variable x y una variable y:")
@@ -131,32 +170,5 @@ etiqueta_variable_y = tk.Label(window, text="VARIABLE Y:")
 etiqueta_variable_y.place(y = 440)
 etiqueta_variable_y.config(bg="#d9ffdf")
 
-# Creación de los Radiobutton
-var1 = StringVar()
-var2 = StringVar()
-
-values = ["longitude", "latitude", "housing_median_age", "total_rooms", "total_bedrooms", "population", "households", "median_income", "median_house_value", "ocean_proximity"]
-    
-for i, value in enumerate(values):
-    
-        rad1 = Radiobutton(window, variable=var1, value=value, text=i, command=Seleccionar)
-        rad2 = Radiobutton(window, variable=var2, value=value, text=i, command=Seleccionar)
-        
-        rad1.pack(side=LEFT)
-        rad2.pack(side=LEFT)
-        
-        rad1.place(x=80 + 120 * i, y=380)
-        rad2.place(x=80 + 120 * i, y=440)
-        
-        rad1.config(bg="#d9ffdf")
-        rad2.config(bg="#d9ffdf")
-
-var1.set(' ')
-var2.set(' ')
-
-button_regresion = tk.Button(window, text="Realizar Regresión Lineal", command=realizar_regresion_button, height=1, width=20)
-button_regresion.place(x=180, y=470)
-
 # Iniciar la aplicación
 window.mainloop() 
-print('lucia')
