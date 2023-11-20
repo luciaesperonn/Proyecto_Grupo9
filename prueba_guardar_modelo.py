93a29299fb38a0ff63aa0a8ca7df18877eea8d4e
Lucía Bardanca <lucia.bardanca@udc.es>
2023-11-20 12:00:42 +0100

    eliminar código de pruebas de mi rama

--- prueba_guardar_modelo.py
+++
@@ -1,8 +0,0 @@
-import joblib
-from regresion_lineal import crear_modelo_regresion_lineal
-
-modelo = crear_modelo_regresion_lineal('housing.csv', ['longitude'], ['latitude'])  # Ejemplo
-
-# Guardar el modelo en un archivo
-joblib.dump(modelo, 'modelo_guardado.joblib')
-
