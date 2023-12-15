c660959eda56439c9e8a4652e577c8d7ceafe072
Aroa <aroa.neira@udc.es>
2023-12-15 16:49:42 +0100

    documentar regresion lineal

--- regresion_lineal.py
+++ regresion_lineal.py
@@ -6,11 +6,32 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
 def verificar_columnas_numericas(datos, columnas):
+    """
+    Verifica que las columnas especificadas en el DataFrame sean de tipo numérico.
+
+    Parámetros:
+    - datos (pd.DataFrame): DataFrame de pandas que contiene los datos.
+    - columnas (list): Lista de nombres de columnas a verificar.
+
+    Lanza:
+    - ValueError: Si alguna columna no es numérica.
+    """
     for col in columnas:
         if not pd.api.types.is_numeric_dtype(datos[col]):
             raise ValueError(f"La columna '{col}' no es numérica.")
 
 def crear_modelo_regresion_lineal(archivo, columna_predictora, columna_objetivo):
+    """
+    Crea un modelo de regresión lineal utilizando scikit-learn.
+
+    Parámetros:
+    - archivo (str): Ruta del archivo que contiene los datos.
+    - columna_predictora (list): Lista con el nombre de la columna predictora.
+    - columna_objetivo (list): Lista con el nombre de la columna objetivo.
+
+    Devuelve:
+    - sklearn.linear_model.LinearRegression: Modelo entrenado de regresión lineal.
+    """
     datos = mostrar_archivos(archivo)
     verificar_columnas_numericas(datos, [columna_predictora[0]] + [columna_objetivo[0]])
 
@@ -36,6 +57,19 @@ def crear_modelo_regresion_lineal(archivo, columna_predictora, columna_objetivo)
     return modelo
 
 def visualizar_modelo(modelo, X, y, etiqueta_x, etiqueta_y):
+    """
+    Visualiza un modelo de regresión lineal utilizando matplotlib.
+
+    Parámetros:
+    - modelo (sklearn.linear_model.LinearRegression): Modelo entrenado de regresión lineal.
+    - X (pd.DataFrame): DataFrame de pandas con la variable predictora.
+    - y (pd.Series): Serie de pandas con la variable objetivo.
+    - etiqueta_x (str): Etiqueta para el eje X.
+    - etiqueta_y (str): Etiqueta para el eje Y.
+
+    Devuelve:
+    - matplotlib.figure.Figure: Figura generada por matplotlib.
+    """
     y_pred = modelo.predict(X)
 
     figure = Figure(figsize=(6, 4))
