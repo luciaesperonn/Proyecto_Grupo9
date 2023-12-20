ddd823f72c09d3a333e8b36cca7c151144a83558
Lucia <lucia.esperon@udc.es>
2023-12-20 18:52:48 +0100

    guardar_modelo

--- clase_modelo.py
+++ clase_modelo.py
@@ -1,10 +1,10 @@
 import joblib
-
+ 
 class ModeloInfo:
-    def __init__(self, x, y, intercept, slope, ecuacion_recta, mse):
+    def __init__(self, x, y, intercept, slope, ecuacion_recta, mse, descripcion):
         """
         Inicia una instancia de la clase ModeloInfo.
-
+ 
         Parámetros:
         - x: Valor de la variable predictora.
         - y: Valor de la variable objetivo.
@@ -19,11 +19,12 @@ class ModeloInfo:
         self.slope = slope
         self.ecuacion_recta = ecuacion_recta
         self.mse = mse
-
+        self.descripcion = descripcion
+ 
     def guardar_modelo(self, file_path):
         """
         Guarda la instancia de la clase en un archivo utilizando joblib.
-
+ 
         Parámetros:
         - file_path (str): Ruta del archivo donde se guardará la instancia.
         """
