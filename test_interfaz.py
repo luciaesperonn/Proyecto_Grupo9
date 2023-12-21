714d257dec50ca3cee163a4012e950de6f22ee15
Lucía Bardanca <lucia.bardanca@udc.es>
2023-12-21 12:57:15 +0100

    unifica las pruebas de funciones del módulo interfaz en un único archivo

--- test_interfaz.py
+++ test_interfaz.py
@@ -1,6 +1,8 @@
 import unittest
 import pandas as pd
-from interfaz import get_first_row 
+import tkinter as tk
+from matplotlib.figure import Figure
+from interfaz import get_first_row, integrar_figura_en_canvas
 
 class TestGetFirstRow(unittest.TestCase):
 
@@ -23,6 +25,31 @@ class TestGetFirstRow(unittest.TestCase):
         # Eliminar el archivo temporal después de la prueba
         import os
         os.remove(file_path)
+
+class TestIntegrarFiguraEnCanvas(unittest.TestCase):
+
+    def test_integrar_figura_en_canvas(self):
+        # Crear una figura de ejemplo
+        figura = Figure(figsize=(5, 4), dpi=100)
+        ax = figura.add_subplot(111)
+        ax.plot([1, 2, 3, 4], [2, 3, 5, 10])
+
+        # Crear una ventana de tkinter
+        ventana = tk.Tk()
+
+        # Llamar a la función que se está probando
+        integrar_figura_en_canvas(figura, ventana)
+
+        # Mostrar la ventana (importante para que los widgets se coloquen correctamente)
+        ventana.update_idletasks()
+
+        # Verificar que la ventana se actualiza correctamente
+        self.assertTrue(ventana.winfo_ismapped())
+
+
+if __name__ == '__main__':
+    unittest.main()
+
 
 if __name__ == '__main__':
     unittest.main()
